7a9e69cd1c11e27858789958c905437bb1900d6c
Juan Sebastian Gomez Vidal <juanquindio@gmail.com>
2024-07-04 17:03:05 -0500

    Delete AntPack

--- AntPack/redux/actions.js
+++
@@ -1,8 +0,0 @@
-export const GET_EMPLOYEE_LIST = 'GET_EMPLOYEE_LIST';
-
-export const getList = list => dispatch => {
-  dispatch({
-    type: GET_EMPLOYEE_LIST,
-    payload: list,
-  });
-};
--- AntPack/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import listReducer from './reducers';
-
-const rootReducer = combineReducers({listReducer});
-
-export const Store = createStore(rootReducer, applyMiddleware(thunk));
